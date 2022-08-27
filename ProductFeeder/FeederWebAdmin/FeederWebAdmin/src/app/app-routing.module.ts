6befbecae84670f8129fdc944646bf7c73fbd7c0
Marco Cantu Gea <marco.cantu.g@gmail.com>
2022-08-26 21:20:48 -0500

    add menu to web front

---
+++ ProductFeeder/FeederWebAdmin/FeederWebAdmin/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ErrorComponent } from './main/error/error.component';
+import { SuppliersComponent } from './pages/suppliers/suppliers.component';
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'suppliers', component: SuppliersComponent },
+  { path: '**', component: ErrorComponent }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(appRoutes)
+  ]
+})
+export class AppRoutingModule { }
--- ProductFeeder/FeederWebAdmin/FeederWebAdmin/src/app/app.module.ts
+++ ProductFeeder/FeederWebAdmin/FeederWebAdmin/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './main/menu/menu.component';
+import { SuppliersComponent } from './pages/suppliers/suppliers.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ErrorComponent } from './main/error/error.component'
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SuppliersComponent,
+    DashboardComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
